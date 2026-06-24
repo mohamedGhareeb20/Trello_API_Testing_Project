e520e134dba20b8533c0e7a032206cf6f8897c17
Dent-taha <tahraf853@gmail.com>
2026-06-25 00:39:36 +0300

    List: add Archive feature

--- TrelloAutomation/src/test/java/com/trello/api/tests/list/TC_012_ArchivingList_Test.java
+++ TrelloAutomation/src/test/java/com/trello/api/tests/list/TC_012_ArchivingList_Test.java
@@ -6,9 +6,6 @@ import com.trello.api.clients.ListClient;
 import com.trello.api.models.request.BoardPayload;
 import com.trello.api.models.request.ListPayload;
 import com.trello.api.tests.BaseTest;
-import io.qameta.allure.Description;
-import io.qameta.allure.Severity;
-import io.qameta.allure.SeverityLevel;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.ITestContext;
@@ -16,16 +13,17 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class TC_011_CreateListTest  extends BaseTest {
+public class TC_012_ArchivingList_Test extends BaseTest {
 
 
     private BoardClient boardClient;
     private ListClient listClient;
     private String tempBoardId;
+    private  String tempListId;
 
 
     @BeforeClass
-    public void setup() {
+    public void setup(ITestContext context) {
         listClient=new ListClient();
         boardClient = new BoardClient();
         BoardPayload payload = BoardPayload.builder()
@@ -37,32 +35,43 @@ public class TC_011_CreateListTest  extends BaseTest {
         ApiAssertions.assertStatusCode(response, 200);
         tempBoardId = response.jsonPath().getString("id");
         logger.info("PRECONDITION: Created temporary Board with ID: {}", tempBoardId);
-    }
 
-    @Test(description = "TC_011 - Verify list creation process")
-    @Description("Verify create list process when selecting a valid Board ID in the URL path and entering valid credentials.")
-    @Severity(SeverityLevel.CRITICAL) // Critical maps to "High" priority inside Allure's standard levels
-    public void CreateListTC(ITestContext context)
-    {
         ListPayload listPayload=ListPayload.builder()
                 .name("Test_List")
                 .idBoard(tempBoardId)
                 .value("true")
                 .build();
 
-        Response response = listClient.createList(listPayload, validRequestSpec);
-        ApiAssertions.assertStatusCode(response, 200);
-        ApiAssertions.assertFieldEquals(response, "name", "Test_List");
-        String listId = response.jsonPath().getString("id");
+        Response listResponse = listClient.createList(listPayload, validRequestSpec);
+        ApiAssertions.assertStatusCode(listResponse, 200);
+        ApiAssertions.assertFieldEquals(listResponse, "name", "Test_List");
+        tempListId=listResponse.jsonPath().getString("id");
+        String listId = listResponse.jsonPath().getString("id");
         Assert.assertNotNull(listId, "List ID was not generated in the response!");
         context.setAttribute("listId", listId);
         System.setProperty("listId", listId);
 
         logger.info("Successfully created List with ID: {}", listId);
-
-
-
     }
+@Test
+public void closeList()
+{
+   Response response= listClient.setListClosed(
+            tempListId,
+            "true",
+            validRequestSpec
+    );
+    ApiAssertions.assertStatusCode(response, 200);
+    ApiAssertions.assertFieldEquals(response, "name", "Test_List");
+    ApiAssertions.assertFieldEquals(response, "closed", true);
+    ApiAssertions.assertFieldEquals(response, "id", tempListId);
+
+}
+
+
+
+
+
     @AfterClass
     public void teardown() {
         boolean isDeletedSuccessfully = false;
